96400ca3195ee422f9d62d8d94d8d8ef95a0d85b
Robert Rappoport <rorappo@gmail.com>
2017-02-07 15:44:25 -0500

    added Button script

---
+++ Gravity Game/Assets/Scripts/buttonColliderGreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonColliderGreen: MonoBehaviour {
+	
+	public GameObject leftDoor;
+	public Material colorChange;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+
+		if (coll.gameObject.tag == "Player1") {
+
+			leftDoor.GetComponent<MeshRenderer> ().material = colorChange;
+
+		}
+
+	}
+}
---
+++ Gravity Game/Assets/Scripts/buttonColliderRed.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonColliderRed: MonoBehaviour {
+
+	public GameObject rightDoor;
+	public Material colorChange;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+
+		if (coll.gameObject.tag == "Player2") {
+
+			rightDoor.GetComponent<MeshRenderer> ().material = colorChange;
+
+		}
+
+	}
+}
