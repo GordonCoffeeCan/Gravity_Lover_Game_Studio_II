ec9545d3771f2425e5262600417caa07f4aaf64b
Mostafa Haque <93haque@gmail.com>
2017-02-07 15:33:33 -0500

    Mostsfafa - Elevator moving up and down

---
+++ Gravity Game/Assets/Scripts/ElevatorScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorScript : MonoBehaviour {
+
+    private float _yPosition;
+    private float _originalY;
+    public float _elevatorHeight;
+
+	// Use this for initialization
+	void Start () {
+
+        _originalY = transform.position.y;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        _yPosition = _originalY + Mathf.PingPong(Time.time, _elevatorHeight);
+
+        transform.position = new Vector3( transform.position.x,_yPosition, transform.position.z);
+    }
+
+}
+
