6907defc91a7ce148f05facd529a5d83b499351e
Robert Rappoport <rorappo@gmail.com>
2017-04-05 21:37:31 -0400

    Scene Transition Script Added
    Implemented in Spoke Level 1

---
+++ Gravity Game/Assets/Scripts/SceneTransitionScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneTransitionScript : MonoBehaviour {
+
+	public bool player1Trigger = false;
+	public bool player2Trigger = false;
+	public string sceneManager;
+
+	void Update (){
+		if (player1Trigger == true && player2Trigger == true) {
+			SceneManager.LoadScene (sceneManager);
+			Debug.Log ("test");
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+
+		if (other.gameObject.tag == "Player1") {
+			player1Trigger = true;
+		
+		}
+
+		if (other.gameObject.tag == "Player2") {
+			player2Trigger = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player1") {
+			player1Trigger = false;
+
+		}
+
+		if (other.gameObject.tag == "Player2") {
+			player2Trigger = false;
+		}
+	}
+
+
+		
+	}
