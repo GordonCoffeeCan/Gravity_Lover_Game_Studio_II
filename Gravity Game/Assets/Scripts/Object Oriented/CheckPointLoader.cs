bacebff672d21d3735a64f1665f723b131f67136
Mostafa Haque <93haque@gmail.com>
2017-04-05 21:15:47 -0400

    Recording transform.position of player 1 and 2 onto a text document.

---
+++ Gravity Game/Assets/Scripts/Object Oriented/CheckPointLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointLoader : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        PlayerLoaderData loadAD = new PlayerLoaderData("Player1Position.txt");
+        PlayerLoaderData loadAD2 = new PlayerLoaderData("Player2Position.txt");
+
+        print(loadAD.position);
+        print(loadAD2.position);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Gravity Game/Assets/Scripts/Object Oriented/CheckPointSaver.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointSaver : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject[] player1Pos = GameObject.FindGameObjectsWithTag("Player1");
+
+        Vector3 pos = player1Pos[0].transform.position;
+        
+
+        PlayerLoaderData ad = new PlayerLoaderData(pos);
+
+        ad.Save("Player1Position.txt");
+
+        //---------------------------------------------------------
+
+        GameObject[] player2Pos = GameObject.FindGameObjectsWithTag("Player2");
+
+        Vector3 pos2 = player2Pos[0].transform.position;
+
+
+        PlayerLoaderData ad2 = new PlayerLoaderData(pos2);
+
+        ad2.Save("Player2Position.txt");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
