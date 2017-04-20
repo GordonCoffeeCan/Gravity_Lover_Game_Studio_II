23d6df6207972678787a1688b1d74847a4a099fd
Mostafa Haque <93haque@gmail.com>
2017-04-20 16:52:49 -0400

    ControlScriptSwitcher implemented

---
+++ Gravity Game/Assets/Scripts/ControllerScripts/ControllerManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerManager : MonoBehaviour {
+
+    private GameObject player1;
+    private GameObject player2;
+
+	// Use this for initialization
+	void Start () {
+        player1 = GameObject.FindWithTag("Player1").gameObject;
+        player2 = GameObject.FindWithTag("Player2").gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            player1.GetComponent<PlayerHoveringController>().enabled = true;
+            player2.GetComponent<PlayerHoveringController>().enabled = true;
+
+            player1.GetComponent<PlayerNewHoverController>().enabled = false;
+            player2.GetComponent<PlayerNewHoverController>().enabled = false;
+
+            player1.GetComponent<BinaryControlScript>().enabled = false;
+            player2.GetComponent<BinaryControlScript>().enabled = false;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            player1.GetComponent<PlayerHoveringController>().enabled = false;
+            player2.GetComponent<PlayerHoveringController>().enabled = false;
+
+            player1.GetComponent<PlayerNewHoverController>().enabled = true;
+            player2.GetComponent<PlayerNewHoverController>().enabled = true;
+
+            player1.GetComponent<BinaryControlScript>().enabled = false;
+            player2.GetComponent<BinaryControlScript>().enabled = false;
+
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            player1.GetComponent<PlayerHoveringController>().enabled = false;
+            player2.GetComponent<PlayerHoveringController>().enabled = false;
+
+            player1.GetComponent<PlayerNewHoverController>().enabled = false;
+            player2.GetComponent<PlayerNewHoverController>().enabled = false;
+
+            player1.GetComponent<BinaryControlScript>().enabled = true;
+            player2.GetComponent<BinaryControlScript>().enabled = true;
+
+        }
+
+    }
+}
