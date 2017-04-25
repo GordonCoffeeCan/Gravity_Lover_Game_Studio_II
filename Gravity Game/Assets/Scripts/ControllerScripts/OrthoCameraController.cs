13389d904ba57ebff5be2cbcafcc7d248a7dd4cd
Mostafa Haque <93haque@gmail.com>
2017-04-25 16:56:58 -0400

    ParallaxScrollingTweaks

---
+++ Gravity Game/Assets/Scripts/ControllerScripts/OrthoCameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrthoCameraController : MonoBehaviour {
+
+    public float GetFieldOfView(float orthoSize, float distanceFromOrigin)
+    {
+        // orthoSize
+        float a = orthoSize;
+        // distanceFromOrigin
+        float b = Mathf.Abs(distanceFromOrigin);
+
+        float fieldOfView = Mathf.Atan(a / b) * Mathf.Rad2Deg * 2f;
+        return fieldOfView;
+
+
+    }
+
+
+}
