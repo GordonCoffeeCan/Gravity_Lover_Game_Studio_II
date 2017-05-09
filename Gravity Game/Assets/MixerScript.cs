f3494d667632bc85b73c019128fc4244afd02a0e
Robert Rappoport <rorappo@gmail.com>
2017-05-09 00:10:03 -0400

    Music System Added

---
+++ Gravity Game/Assets/MixerScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MixerScript : MonoBehaviour {
+	public AudioMixerSnapshot Music1;
+	public AudioMixerSnapshot Music2;
+	public AudioMixerSnapshot Music3;
+	public AudioMixerSnapshot FadeToBlack;
+	public AudioSource MainTheme;
+	public AudioSource PlayerATheme;
+	public AudioSource PlayerBTheme;
+	public bool PlayerACross;
+	public bool PlayerBCross;
+	public bool blackFade;
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+		if (PlayerACross)
+		{
+			PlayerACross = false;
+			PlayerBCross = false;
+			PlayerATheme.Play ();
+			Music2.TransitionTo (3);
+			StartCoroutine (WaitTimeA ());
+		}
+
+		if (PlayerBCross)
+		{
+			PlayerBCross = false;
+			PlayerACross = false;
+			PlayerBTheme.Play ();
+			Music3.TransitionTo (3);
+			StartCoroutine (WaitTimeB ());
+		}
+
+		if (blackFade) 
+		{
+			Debug.Log ("fade to black");
+			PlayerBCross = false;
+			PlayerACross = false;
+			FadeToBlack.TransitionTo (2);
+		}
+			
+	}
+
+	IEnumerator WaitTimeA ()
+	{
+		int wait_time = Random.Range (8,14);
+		yield return new WaitForSeconds (wait_time);
+		Music1.TransitionTo (3);
+	}
+
+	IEnumerator WaitTimeB ()
+	{
+		int wait_time = Random.Range (8,14);
+		yield return new WaitForSeconds (wait_time);
+		Music1.TransitionTo (3);
+	}
+	}
--- Gravity Game/Assets/Scripts/Object Oriented/CheckPointCollision.cs
+++ Gravity Game/Assets/Scripts/Object Oriented/CheckPointCollision.cs
@@ -13,6 +13,7 @@ public class CheckPointCollision : MonoBehaviour {
     public GameObject player1SpawnPoint;
     public GameObject player2SpawnPoint;
 
+
     // Use this for initialization
     void Start () {
 		
@@ -22,7 +23,13 @@ public class CheckPointCollision : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D player)
     {
         if (player.gameObject.tag == "Player1" || player.gameObject.tag == "Player2")
-        {
+		{
+			if (player.gameObject.tag == "Player1") 
+			{
+				GameObject.Find ("MusicSource").GetComponent<MixerScript> ().PlayerACross = true;
+			}
+			else
+				GameObject.Find ("MusicSource").GetComponent<MixerScript> ().PlayerBCross = true;
 
             pos1 = player1SpawnPoint.transform.position;
             pos2 = player2SpawnPoint.transform.position;
